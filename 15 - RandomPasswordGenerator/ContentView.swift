c8109a26b1176db06222a112a6d9e8cc14356d71
Whoopinstick <k.m.paul68@gmail.com>
2020-09-06 15:28:05 -0400

    Initial Commit

---
+++ 15 - RandomPasswordGenerator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  15 - RandomPasswordGenerator
+//
+//  Created by Kevin Paul on 9/6/20.
+//  Copyright © 2020 Whoopinstick. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
